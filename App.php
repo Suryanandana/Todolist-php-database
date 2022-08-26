9dc9ca58ef6e883dc4ccbfb3b55715aef5e9a5ba
Suryanandana <suryanandana85@gmail.com>
2022-08-27 03:12:45 +0800

    include file yang terdapat pada directory Logic ke App

--- App.php
+++ App.php
@@ -1,4 +1,7 @@
 <?php
 require_once "Model/TodoList.php";
+require_once "Logic/ShowTodoList.php";
+require_once "Logic/AddTodoList.php";
+require_once "Logic/RemoveTodoList.php";
 
 echo "TodoList";
