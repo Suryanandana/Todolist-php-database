8f3792252a0334e43e16c99b3b604816c8b79769
Suryanandana <suryanandana85@gmail.com>
2022-08-27 03:44:43 +0800

    membuat unit test untuk logic ShowTodoList

---
+++ Test/TestShowTodoList.php
@@ -0,0 +1,9 @@
+<?php
+require_once "../Model/TodoList.php";
+require_once "../Logic/ShowTodoList.php";
+
+$todolist[1] = "Tugas Kuliah";
+$todolist[2] = "Tugas Rumah";
+$todolist[3] = "Tugas Organisasi";
+
+showTodoList();
