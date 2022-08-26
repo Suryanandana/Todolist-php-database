488b8d26abc82274b35884d6500196df9ecfa7dd
Suryanandana <suryanandana85@gmail.com>
2022-08-27 03:15:37 +0800

    include file yang terdapat pada directory View ke App

--- App.php
+++ App.php
@@ -3,5 +3,8 @@ require_once "Model/TodoList.php";
 require_once "Logic/ShowTodoList.php";
 require_once "Logic/AddTodoList.php";
 require_once "Logic/RemoveTodoList.php";
+require_once "View/ViewShowTodoList.php";
+require_once "View/ViewAddTodoList.php";
+require_once "View/ViewRemoveTodoList.php";
 
 echo "TodoList";
