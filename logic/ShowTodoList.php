85be5a5a4312dd5f2fe3bd198eaa33e477e830a4
Suryanandana <suryanandana85@gmail.com>
2022-08-27 03:43:27 +0800

    membuat perulangan untuk menampilkan array todolist

--- logic/ShowTodoList.php
+++ logic/ShowTodoList.php
@@ -1,5 +1,11 @@
 <?php
 // Menampilkan Todo di List
 function showTodoList(){
+    global $todolist;
 
+    echo "TODOLIST:".PHP_EOL;
+
+    foreach($todolist as $number => $value){
+        echo "$number. $value".PHP_EOL;
+    }
 }
