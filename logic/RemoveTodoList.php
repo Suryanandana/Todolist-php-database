5ece63631d70bb0ce969c8bb3dade80b39ea1e2c
Suryanandana <suryanandana85@gmail.com>
2022-08-29 21:38:19 +0800

    membuat hapus todolist

--- logic/RemoveTodoList.php
+++ logic/RemoveTodoList.php
@@ -1,5 +1,18 @@
 <?php
 // Menghapus Todo di List
-function removeTodoList(){
-    
+function removeTodoList($number): bool{
+    global $todolist;
+
+    if ($number > sizeof($todolist)){
+        return false;
+    }
+
+    while($number < sizeof($todolist)){
+        $todolist[$number] = $todolist[$number + 1];
+        $number++;
+    }
+
+    unset($todolist[sizeof($todolist)]);
+
+    return true;
 }
