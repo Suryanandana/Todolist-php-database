ff27bfe58166e00760aa47d40d3a3ad7b1a10ede
Suryanandana <suryanandana85@gmail.com>
2022-08-29 23:00:41 +0800

    membuat view add todolist

--- View/ViewAddTodoList.php
+++ View/ViewAddTodoList.php
@@ -1,5 +1,17 @@
 <?php
+
+require_once '../Model/TodoList.php';
+require_once '../Helper/Input.php';
+require_once '../Logic/AddTodoList.php';
+
 // Tampilan Menambah Todo di List
 function viewAddTodoList(){
+    $todo = input('Tambah todolis (x jika batal)');
+
+    if(trim($todo) == 'x'){
+        echo "Batal menambah todo";
+    } else {
+        addTodoList($todo);
+    }
 
 }
