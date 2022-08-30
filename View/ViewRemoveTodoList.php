1721d7397140b33c9a7f6eb82bf4e66c474082c4
Suryanandana <suryanandana85@gmail.com>
2022-08-30 12:26:01 +0800

    menambah view remove todolist

--- View/ViewRemoveTodoList.php
+++ View/ViewRemoveTodoList.php
@@ -1,5 +1,23 @@
 <?php
 // Tampilan Hapus Todo di List
+require_once '../Model/TodoList.php';
+require_once '../Helper/Input.php';
+require_once '../Logic/RemoveTodoList.php';
+
 function viewRemoveTodoList(){
     
+    echo 'HAPUS TODOLIST'.PHP_EOL;
+    $todo = input('Hapus todolist (x jika batal)');
+
+    if(trim($todo) == 'x'){
+        echo 'Batal menghapus todolist' . PHP_EOL;
+    } else {
+        $success = removeTodoList($todo);
+        if($success){
+            echo "Berhasil menghapus todo $todo" . PHP_EOL;
+        } else {
+            echo "Gagal menghapus todo" . PHP_EOL;
+        }
+    }
+
 }
