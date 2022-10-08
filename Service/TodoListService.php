d5fea11c1bec6c79d4823fb025ab4f2c08b86de0
Suryanandana <suryanandana85@gmail.com>
2022-10-09 02:11:45 +0800

    membuat Service

---
+++ Service/TodoListService.php
@@ -0,0 +1,16 @@
+<?php 
+namespace Service {
+
+    interface TodoListService {
+
+        function showTodoList(): void;
+
+        function addTodoList(string $todo): void;
+
+        function removeTodoList(int $number): void;
+
+    }
+
+}
+
+?>
