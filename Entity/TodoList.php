00273963b2d72353b8680a3417d0522164f8251d
Suryanandana <suryanandana85@gmail.com>
2022-10-09 01:57:20 +0800

    membuat Entity

---
+++ Entity/TodoList.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Entity {
+    class TodoList
+    {
+        private string $todo;
+
+        public function __construct(string $todo = "")
+        {
+            $this->todo = $todo;
+        }
+
+        public function getTodo(){
+            return $this->todo;
+        }
+        
+        public function setTodo(string $todo){
+            $this->todo = $todo;
+        }
+    }
+}
+
+?>
