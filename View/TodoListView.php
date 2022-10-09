3b487c666a4f11b39aac2d539eaf115539469b68
Suryanandana <suryanandana85@gmail.com>
2022-10-09 14:02:34 +0800

    menambah view show todolist

--- View/TodoListView.php
+++ View/TodoListView.php
@@ -1,10 +1,33 @@
 <?php 
 namespace View {
 
+    use Service\TodoListService;
+
     class TodoListView {
-
+        private TodoListService $todoListService;
         function showTodoList(): void {
+            while(true){
 
+                $this->todoListService->showTodoList();
+            
+                echo "Menu".PHP_EOL;
+                echo "1 Tambah Todo".PHP_EOL;
+                echo "2 Hapus Todo".PHP_EOL;
+                echo "x Keluar Todo".PHP_EOL;
+            
+                $result = input("Pilih").PHP_EOL;
+                    
+                if($result == "1"){
+                    $this->addTodoList();
+                } else if($result == "2"){
+                    $this->removeTodoList();
+                } else if(trim($result) == "x"){
+                    break;
+                } else {
+                    echo "Perintah tidak dimengerti".PHP_EOL;
+                }
+            
+            }
         }
 
         function addTodoList(): void {
